7a12561175289dfc9c46a01c714f1dac4598f92b
Adrian Ordonez Tostega <adriantostega@hotmail.com>
2018-01-28 22:59:00 -0800

    BTT - ATOS - Added TrackingRecordRepositoryTest class to create the test for that class but not implemented any test.

---
+++ BasicTimeTrackerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicTimeTrackerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicTimeTrackerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec6d545a-4d55-43e2-8046-b06e1832a355")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
